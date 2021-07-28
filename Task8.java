b3fce0093e418315cc7fcfcfdedfc8f8761a0afc
YaroslavaMO <murometcya@gmail.com>
2021-07-28 13:52:18 +0300

    add Task8

---
+++ Task8.java
@@ -0,0 +1,13 @@
+public class Task8 {
+    public static void main(String[] args) {
+        Auto auto1 = new Auto("BMW");
+    }
+}
+    class Auto {
+    String model;
+
+    public Auto (String model){
+        System.out.println(model);
+    }
+}
+
