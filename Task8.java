df1e8a2a51412487bd075db55c1ca97a8769be8c
YaroslavaMO <murometcya@gmail.com>
2021-08-05 11:58:54 +0300

    Task8 correction

--- Task8.java
+++ Task8.java
@@ -1,13 +1,15 @@
-public class Task8 {
+public class Task82 {
     public static void main(String[] args) {
         Auto auto1 = new Auto("BMW");
     }
-}
-    class Auto {
-    String model;
+    static class Auto {
+        String model;
 
-    public Auto (String model){
-        System.out.println(model);
+        public Auto (String model) {
+            this.model = model;
+            System.out.println( model);
+        }
+
     }
 }
 
